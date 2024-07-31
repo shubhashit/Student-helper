3499780f55368b46e465bc1997af86187301a898
Shubhashit <shubhashitjain@gmail.com>
2024-07-31 15:30:37 +0530

    Subjects added

--- src/components/Subjects.js
+++ src/components/Subjects.js
@@ -10,11 +10,33 @@ export default function Subjects() {
           <div className="notiborderglow"></div>
           <div className="notititle">Power Electronics</div>
           <div className="notibody">Sushma Gupta</div>
-          <div className='absolute z-10 w-[20%] min-h-fit h-8 border right-4 tickmark border-[#85f5b4] rounded-md flex flex-row text-sm items-center justify-center'>
+          {/* <div className='absolute z-10 w-[20%] min-h-fit h-8 border right-4 tickmark border-[#85f5b4] rounded-md flex flex-row text-sm items-center justify-center'>
             <div className='w-1/2 flex items-center justify-center h-full text-[#58bc82] hover:bg-[#58bc82] hover:text-[#18181b] hover:font-bold'>Yes</div>
             <div className='border border-[#85f5b4] h-full'></div>
             <div className='w-1/2 flex items-center justify-center h-full text-[#58bc82] hover:bg-[#58bc82] hover:text-[#18181b] hover:font-bold' >No</div>
-          </div>
+          </div> */}
+        </div>
+        <div className="notification relative mt-2 w-[90%] max-md:w-full">
+          <div className="notiglow"></div>
+          <div className="notiborderglow"></div>
+          <div className="notititle">Hum-454</div>
+          <div className="notibody">MD</div>
+          {/* <div className='absolute z-10 w-[20%] min-h-fit h-8 border right-4 tickmark border-[#85f5b4] rounded-md flex flex-row text-sm items-center justify-center'>
+            <div className='w-1/2 flex items-center justify-center h-full text-[#58bc82] hover:bg-[#58bc82] hover:text-[#18181b] hover:font-bold'>Yes</div>
+            <div className='border border-[#85f5b4] h-full'></div>
+            <div className='w-1/2 flex items-center justify-center h-full text-[#58bc82] hover:bg-[#58bc82] hover:text-[#18181b] hover:font-bold' >No</div>
+          </div> */}
+        </div>
+        <div className="notification relative mt-2 w-[90%] max-md:w-full">
+          <div className="notiglow"></div>
+          <div className="notiborderglow"></div>
+          <div className="notititle">Open Elective</div>
+          <div className="notibody">N.A.</div>
+          {/* <div className='absolute z-10 w-[20%] min-h-fit h-8 border right-4 tickmark border-[#85f5b4] rounded-md flex flex-row text-sm items-center justify-center'>
+            <div className='w-1/2 flex items-center justify-center h-full text-[#58bc82] hover:bg-[#58bc82] hover:text-[#18181b] hover:font-bold'>Yes</div>
+            <div className='border border-[#85f5b4] h-full'></div>
+            <div className='w-1/2 flex items-center justify-center h-full text-[#58bc82] hover:bg-[#58bc82] hover:text-[#18181b] hover:font-bold' >No</div>
+          </div> */}
         </div>
       </div>
     </div>
